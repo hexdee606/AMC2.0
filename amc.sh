3b047b05bd5951159140e8b50bd8a9b2ee7e1ee3
Dipen Chavan <dipenc245@gmail.com>
2021-09-29 14:24:54 +0530

    amc.sh build-290921-0223P
    [x] Fix spell missing.
    [x] Add new function.
    [x] Improvement in script

--- amc.sh
+++ amc.sh
@@ -1,18 +1,36 @@
 #!/bin/bash
 
-echo '############################################################'
-echo '#   Automatic Media Access Control [MAC] Address Changer   #'
-echo '############################################################'
-echo '# Choose # For                                             #'
-echo '############################################################'
-echo '# 1      # Change MAC Address Automatically                #'
-echo '# 2      # Display Available Network Interfaces            #'
-echo '# 3      # About Alchemists                                #'
-echo '# 4      # Join Alchemists                                 #'
-echo '# 5      # Version                                         #'
-echo '# 0      # Exit                                            #'
-echo '############################################################'
+while true; do
+
+clear
+sudo clear
+
+echo '######################################################################'
+echo '#                   _/_/        _/      _/        _/_/_/             #'
+echo '#                _/    _/      _/_/  _/_/      _/                    #'
+echo '#               _/_/_/_/      _/  _/  _/      _/                     #'
+echo '#              _/    _/      _/      _/      _/                      #'
+echo '#             _/    _/      _/      _/        _/_/_/                 #'
+echo '#                                                                    #'
+echo '#             Coded  by Dipen   Chavan   - @hexdee606                #'
+echo '#             Tested by Aniket  Kumbhar  - @itachi-91                #'
+echo '#             Tested by Ankita  Hendre   - @athena-077               #'
+echo '#             Tested by Shreyas Kulkarni - @paradox44                #'
+echo '######################################################################'
+echo '#     Automatic Media Access Control [MAC] Address Spoofig Tool      #'
+echo '######################################################################'
+echo '# Choose # For                                                       #'
+echo '######################################################################'
+echo '# 1      # Automatic Media Access Control [MAC] Address Spoofing     #'
+echo '# 2      # Available Network Interfaces                              #'
+echo '# 3      # About Alchemists Community                                #'
+echo '# 4      # Join Alchemists Community                                 #'
+echo '# 5      # CC0 1.0 Universal License                                 #'
+echo '# 6      # Version                                                   #'
+echo '# 0      # Exit                                                      #'
+echo '######################################################################'
 read input_option
+
 
 case $input_option in
 
@@ -23,45 +41,86 @@ case $input_option in
 #case 1
 "1")  clear
       sudo clear
-      echo '############################################################'
-      echo '#   Automatic Media Access Control [MAC] Address Changer   #'
-      echo '############################################################'
-      echo '# Please enter network interface name [Ex. eth0] :  ' 
+      echo '######################################################################'
+      echo '#                   _/_/        _/      _/        _/_/_/             #'
+      echo '#                _/    _/      _/_/  _/_/      _/                    #'
+      echo '#               _/_/_/_/      _/  _/  _/      _/                     #'
+      echo '#              _/    _/      _/      _/      _/                      #'
+      echo '#             _/    _/      _/      _/        _/_/_/                 #'
+      echo '#                                                                    #'
+      echo '#             Coded  by Dipen   Chavan   - @hexdee606                #'
+      echo '#             Tested by Aniket  Kumbhar  - @itachi-91                #'
+      echo '#             Tested by Ankita  Hendre   - @athena-077               #'
+      echo '#             Tested by Shreyas Kulkarni - @paradox44                #'
+      echo '######################################################################'
+      echo '#     Automatic Media Access Control [MAC] Address Spoofig Tool      #'
+      echo '######################################################################'
+      echo '# Please enter network interface name [Ex. eth0] :                   #' 
       read ntwrk_inter
-      echo '############################################################' 
-      echo '# Please enter interval time [between 25 to 60 ] :  '
+      echo '######################################################################' 
+      echo '# Please enter interval time [between 25 to 60 ] :                   #'
       read intrvl_tm
-      echo '############################################################'
+      echo '######################################################################'
       sudo python3 amc.py -i $ntwrk_inter -t $intrvl_tm
+      echo "Press Enter to continue."
+      read wt
 ;;
 
 #case 2
 "2") sudo python3 amc.py -n True
+echo "Press Enter to continue."
+read wt
 ;;
 
 #case 3
 "3") sudo python3 amc.py -a True
+echo "Press Enter to continue."
+read wt
 ;;
 
 #case 4
 "4") sudo python3 amc.py -j True
+echo "Press Enter to continue."
+read wt
 ;;
 
 #case 5
-"5") sudo python3 amc.py -v True
+"5") sudo python3 amc.py -l True
+echo "Press Enter to continue."
+read wt
+;;
+
+#case 6
+"6") sudo python3 amc.py -v True
+echo "Press Enter to continue."
+read wt
 ;;
 
 *)  
       clear
       sudo clear
-      echo '############################################################'
-      echo '#   Automatic Media Access Control [MAC] Address Changer   #'
-      echo '############################################################'
-      echo '# ERROR : PLEASE ENTER VALID OPTION                        #'
-      echo '############################################################' 
+      echo '######################################################################'
+      echo '#                   _/_/        _/      _/        _/_/_/             #'
+      echo '#                _/    _/      _/_/  _/_/      _/                    #'
+      echo '#               _/_/_/_/      _/  _/  _/      _/                     #'
+      echo '#              _/    _/      _/      _/      _/                      #'
+      echo '#             _/    _/      _/      _/        _/_/_/                 #'
+      echo '#                                                                    #'
+      echo '#             Coded  by Dipen   Chavan   - @hexdee606                #'
+      echo '#             Tested by Aniket  Kumbhar  - @itachi-91                #'
+      echo '#             Tested by Ankita  Hendre   - @athena-077               #'
+      echo '#             Tested by Shreyas Kulkarni - @paradox44                #'
+      echo '######################################################################'
+      echo '#     Automatic Media Access Control [MAC] Address Spoofig Tool      #'
+      echo '######################################################################'
+      echo '# ERROR AMCSH0000x000001 : Please select valid option.               #'
+      echo '######################################################################'
+      echo "Press Enter to continue."
+      read wt
 ;;
+
 esac
 
-echo "Press Enter to Exit code."
-read wt
+done 
+
 exit 1
